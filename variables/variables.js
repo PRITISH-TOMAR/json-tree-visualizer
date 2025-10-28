cc709fd542dffe5fd63e514ef0e41840cbaf4579
Pritish Tomar <pritishtomar2004@gmail.com>
2025-10-28 22:49:36 +0530

    add-variables-to-project

---
+++ variables/variables.js
@@ -0,0 +1,29 @@
+export const COLOR_VAR_NAMES = {
+  OBJECT: '--color-object',
+  ARRAY: '--color-array',
+  PRIMITIVE: '--color-primitive',
+  BG: '--color-bg',
+  TEXT: '--color-text',
+};
+
+export const THEMES = {
+  LIGHT: 'light',
+  DARK: 'dark',
+  OCEAN: 'ocean',
+  SUNSET: 'sunset',
+};
+
+
+export const LOCAL_STORAGE_KEYS = {
+    THEME: 'app-theme',
+}
+
+export const DEFAULTS = {
+    THEME: THEMES.LIGHT,
+}
+
+export const DEFAULT_COLORS = {
+    OBJECT: '#6366F1',
+    ARRAY: '#22C55E',
+    PRIMITIVE: '#F59E0B'
+}
